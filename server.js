bcb6549260bd2e734ff11628fff9d7f454a00cb0
Charan731 <84834271+Charan731@users.noreply.github.com>
2025-03-04 11:59:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const MONGO_URI = process.env.MONGO_URI;
 const RAZORPAY_SECRET = process.env.RAZORPAY_SECRET;
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+    
     .then(() => console.log("✅ MongoDB connected"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 
